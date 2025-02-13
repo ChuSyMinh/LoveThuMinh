2742a522279ffd83e86d588d6cb4c7a47cdeebae
Chu Sỹ Minh <minhaevchhtb@gmail.com>
2025-02-13 10:24:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
-    titleWeb: "LOVE Thu MinhMinh",
-    introTitle: 'Hello MMinh nhé',
+    titleWeb: "LOVE Thu Minh",
+    introTitle: 'Hello Minh nhé',
     introDesc: `Anh có điều này muốn hỏi, em nhớ trả lời thật lòng nha :3`,
     btnIntro: '^^Dạ^^',
-    title: 'Có Phải MMinh yêu anh nhiều lắm phải không nào 🥰',
+    title: 'Có Phải Minh yêu anh nhiều lắm phải không nào 🥰',
     desc: 'Không nói coi như là em rất yêu anh đó nha ',
     btnYes: 'Yêu ơi là yêu <33',
     btnNo: 'Không nha :3',
@@ -11,7 +11,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu anh không cần lý do. em yêu anh lắm',
     mess: 'Anh biết mà 🥰. Yêu em nhiều lắm 😘😘',
-    messDesc: 'Chúc em một ngày 14/02 vui vẻ',
+    messDesc: 'Chúc em một ngày valentine vui vẻ',
     btnAccept: 'Dạ',
-    messLink: 'https://www.facebook.com/minh.chusy.9' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://chusyminh.github.io/heart/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
